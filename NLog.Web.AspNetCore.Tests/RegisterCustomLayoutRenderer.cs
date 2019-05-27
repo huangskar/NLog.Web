fbcd5f9fbfe21d37f5cfd283362dd2ffe8b645c2
Julian Verdurmen <304NotModified@users.noreply.github.com>
2019-05-28 00:09:38 +0200

    test2

--- NLog.Web.AspNetCore.Tests/RegisterCustomLayoutRenderer.cs
+++ NLog.Web.AspNetCore.Tests/RegisterCustomLayoutRenderer.cs
@@ -3,9 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+
+
 using NLog.Layouts;
+#if ASP_NET_CORE
+using Microsoft.AspNetCore.Http;
 using NLog.Web.DependencyInjection;
+#else
+using System.Web;
+#endif
 using NLog.Web.LayoutRenderers;
 using NLog.Web.Tests.LayoutRenderers;
 using NSubstitute;
@@ -19,25 +25,45 @@ namespace NLog.Web.AspNetCore.Tests
         public void RegisterLayoutRendererTest()
         {
             var httpcontext = SetupHttpAccessorWithHttpContext();
+#if ASP_NET_CORE
             httpcontext.Connection.LocalPort.Returns(123);
+#else
+            httpcontext.Request.RawUrl.Returns("123");
+#endif
 
             // Act
-            AspNetLayoutRendererBase.Register("test-web", 
-                (logEventInfo, httpContextAccessor, loggingConfiguration) => httpContextAccessor.HttpContext.Connection.LocalPort);
+            AspNetLayoutRendererBase.Register("test-web",
+                (logEventInfo, httpContextAccessor, loggingConfiguration) =>
+#if ASP_NET_CORE
+                    httpContextAccessor.HttpContext.Connection.LocalPort);
+#else
+                    httpContextAccessor.HttpContext.Request.RawUrl);
+#endif
             Layout l = "${test-web}";
             var restult = l.Render(LogEventInfo.CreateNullEvent());
 
             // Assert
             Assert.Equal("123", restult);
         }
-      
-        private static HttpContext SetupHttpAccessorWithHttpContext()
+
+        private static 
+            #if ASP_NET_CORE
+            HttpContext 
+#else
+            HttpContextBase
+            #endif
+            
+            SetupHttpAccessorWithHttpContext()
         {
             var httpContextAccessorMock = Substitute.For<IHttpContextAccessor>();
             var serviceProviderMock = Substitute.For<IServiceProvider>();
             serviceProviderMock.GetService(typeof(IHttpContextAccessor)).Returns(httpContextAccessorMock);
 
+#if ASP_NET_CORE
             var httpcontext = Substitute.For<HttpContext>();
+#else
+            var httpcontext = Substitute.For<HttpContextBase>();
+#endif
 
             ServiceLocator.ServiceProvider = serviceProviderMock;
             httpContextAccessorMock.HttpContext.Returns(httpcontext);
