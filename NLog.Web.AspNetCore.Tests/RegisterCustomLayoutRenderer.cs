59f302c233bd43c7f398a1d37940e40d72e563ed
Julian Verdurmen <304NotModified@users.noreply.github.com>
2019-05-27 23:52:55 +0200

    signing

--- NLog.Web.AspNetCore.Tests/RegisterCustomLayoutRenderer.cs
+++ NLog.Web.AspNetCore.Tests/RegisterCustomLayoutRenderer.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using NLog.Layouts;
+using NLog.Web.DependencyInjection;
 using NLog.Web.LayoutRenderers;
 using NLog.Web.Tests.LayoutRenderers;
+using NSubstitute;
 using Xunit;
 
 namespace NLog.Web.AspNetCore.Tests
@@ -16,11 +19,18 @@ namespace NLog.Web.AspNetCore.Tests
         public void RegisterLayoutRendererTest()
         {
 
-            //ServiceLocator.ServiceProvider;
 
+            var httpContextAccessorMock = Substitute.For<IHttpContextAccessor>();
+            var serviceProviderMock = Substitute.For<IServiceProvider>();
+            serviceProviderMock.GetService(typeof(IHttpContextAccessor)).Returns(httpContextAccessorMock);
+            ServiceLocator.ServiceProvider = serviceProviderMock;
+
+            // Act
             AspNetLayoutRendererBase.Register("test-web", (info, accessor, arg3) => accessor.HttpContext.Connection.LocalPort);
             Layout l = "${test-web}";
             var restult = l.Render(LogEventInfo.CreateNullEvent());
+
+            // Assert
             Assert.Equal("something", restult);
 
         }
